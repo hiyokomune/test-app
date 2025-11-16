0a40e29f3811eb2ef3de9c51dc837a107958d39a
hiyokomune <hiyokomune@gmail.com>
2025-11-16 16:26:27 +0900

    HeaderのリンクがaタグからLinkに変更

--- test-app/src/Header.jsx
+++ test-app/src/Header.jsx
@@ -1,10 +1,12 @@
 import styles from'./Header.module.css';
+import { Link } from "react-router-dom";
+
 
 export default function Header() {
   return (
   <header className={styles.header}>
-      <a href='/' className={styles.headerLink}>Blog</a>
-      <a href='' className={styles.headerLink}>お問い合わせ</a>
+      <Link to = '/' className={styles.headerLink}>Blog</Link>
+      <Link to = '/' className={styles.headerLink}>お問い合わせ</Link>
     </header>
   )
 }
